7e6d1f942aa861ffcd2a2f7b57518740bc40030d
Muhamad Badruni <badrunimuhammad@gmail.com>
2023-02-17 17:25:20 +0700

    coba update

--- 6. Looping Statement/tugas3v2.js
+++ 6. Looping Statement/tugas3v2.js
@@ -1,11 +1,11 @@
 //menentukan variable
 let nama = "KUNCIE"
-const numRows = 8;
+const numRows = 7;
 //looping variable nama
 for (let z = 0; z < numRows; z++) {
   nama += nama;
 }
-//print input
+//print total input
 console.log("Input :"+numRows);
 //looping row
 for (let i = 0; i <= numRows-1; i++) {
